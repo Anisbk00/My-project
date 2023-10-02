87f7b110e2d510d01aa8d5ea2a18c8182735df8f
Anisbk00 <anisbenkhalifa87@gmail.com>
2023-10-02 19:27:08 +0100

    project1

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");
+});
+
+// making the scroll of the bottom smoother using scrool behaviour
+document.querySelector(".scroll-btn").addEventListener("click",()=>{
+    document.querySelector("html").style.scrollBehavior="smooth";
+    setTimeout(()=>{
+        document.querySelector("html").style.scrollBehavior="unset";
+    },1000)
+})
+
+
+window.onload=()=>{
+    setTimeout(()=>{
+        document.querySelector('body').classList.add('display')
+    },4000)
+}
